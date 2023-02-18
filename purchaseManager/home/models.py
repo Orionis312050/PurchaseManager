377f7cc40745c7ae6e25bab8dca3577fdf48368f
Raffier Ugo <gou.raffier@gmail.com>
2023-02-18 01:13:19 +0100

    2eme étape

--- purchaseManager/home/models.py
+++ purchaseManager/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=200)
+    desc = models.TextField()
+    auth = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
